89471d5f485ccd0d6f74709380813cd46c7e4291
sandra-212 <47017679+sandra-212@users.noreply.github.com>
2022-07-04 16:22:39 -0400

    fetch repos backend

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,4 +1,6 @@
 import { Router, Request, Response } from 'express';
+import fileRepos from '../../data/repos.json';
+const axios = require('axios');
 
 export const repos = Router();
 
@@ -7,6 +9,19 @@ repos.get('/', async (_: Request, res: Response) => {
 
   res.status(200);
 
+  axios
+    .get(' https://api.github.com/users/silverorange/repos')
+    .then(function (response: any) {
+      // handle success
+      let remoteRepos = response.data;
+      let repos = [...fileRepos, ...remoteRepos].filter(
+        (repo: any) => repo.fork === false
+      );
+      res.json(repos);
+    })
+    .catch(function (error: any) {
+      // handle error
+      console.log(error);
+    });
   // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
 });
