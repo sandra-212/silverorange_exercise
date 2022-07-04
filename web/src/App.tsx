ea48c0b91b0b778cab0bb9c052bff605700c347f
sandra-212 <47017679+sandra-212@users.noreply.github.com>
2022-07-04 16:44:56 -0400

    fetch repos frontend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,25 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 export function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [repos, setRepos] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/repos')
+      .then((res) => res.json())
+      .then((data) => setRepos(data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(repos);
+  return <ul>{repos.map((repo) => repo.id)}</ul>;
 }
